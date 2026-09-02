e08d3507e92fce7b4ac457967ec76ee7f30a153f
Priyadharshan S <priyadharshan.s2026@vitstudent.ac.in>
2026-09-02 07:41:05 +0530

    Create    index.ts

---
+++ frontend/api/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import { createServer } from "http";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const staticPath = path.resolve(__dirname, "..", "dist", "public");
+
+app.use(express.static(staticPath));
+
+app.get("*", (_req, res) => {
+  res.sendFile(path.join(staticPath, "index.html"));
+});
+
+export default app;
